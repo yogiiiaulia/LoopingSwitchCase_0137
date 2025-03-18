ea7711d8b5abbb184f96a0209626c1234a8ea367
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-03-18 19:36:32 +0700

    program perulangan while dan do while

---
+++ vs2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    //perulangan while
+    int x;
+    int perulanganWhile = 0;
+    int perulanganDo = 0;
+
+    srand(time(0));
+    x = rand() % 10;
+    cout << "Perulangan While " << endl;
+    cout << "Nilai x awal = " << endl;
+
+    while (x < 7) {
+        cout << "Nilai X = " << x << " Lari Keliling " << endl;
+        x = rand() % 10;
+        perulanganWhile++;
+    }
+
+    cout << "Nilai x Terakhir = " << x << endl;
+    cout << "Jumlah Perulangan = " << perulanganWhile << endl;
+}
