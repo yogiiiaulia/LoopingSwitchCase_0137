d228ce81113d7739acae65291a36b0e153c53073
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-03-18 18:57:02 +0700

    pembuatan program pengulangan for

---
+++ vs.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //pencacah naik
+    int i;
+    for ( i = 0; i < 5; i++) {
+        cout << "Nilai i = "<< i <<"Lari Keliling Lapangan" << endl;
+    }
+    cout << "Nilai i Terakhir : " << i << endl;
+
+    //pencacah turun
+    for (i = 5; i > 0; i--){
+        cout << "Nilai i = "<< "Lari Keliling Lapangan" << endl;
+    }
+    cout << "Nilai i Terakhir : "<< i << endl;
+}
